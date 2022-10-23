778a79ee051dfd06274e7d616feb2b3decfb304c
danya <dnosyk@cfg.com.ua>
2022-10-23 17:28:12 +0300

    first commit

---
+++ bpm_to_ble.ino
@@ -0,0 +1,55 @@
+
+/*  Getting_BPM_to_Monitor prints the BPM to the Serial Monitor, using the least lines of code and PulseSensor Library.
+ *  Tutorial Webpage: https://pulsesensor.com/pages/getting-advanced
+ *
+--------Use This Sketch To------------------------------------------
+1) Displays user's live and changing BPM, Beats Per Minute, in Arduino's native Serial Monitor.
+2) Print: "♥  A HeartBeat Happened !" when a beat is detected, live.
+2) Learn about using a PulseSensor Library "Object".
+4) Blinks LED on PIN 13 with user's Heartbeat.
+--------------------------------------------------------------------*/
+
+#define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math.
+#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
+#include <SoftwareSerial.h>
+#include <stdio.h>
+#include <string.h>
+
+//  Variables
+const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+const int LED13 = 13;          // The on-board Arduino LED, close to PIN 13.
+int Threshold = 570;           // Determine which Signal to "count as a beat" and which to ignore.
+                               // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
+                               // Otherwise leave the default "550" value. 
+                               
+PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
+SoftwareSerial blePort(8,9);
+
+
+void setup() {   
+
+  blePort.begin(9600);          // For Serial Monitor
+  Serial.begin(9600);   
+  // Configure the PulseSensor object, by assigning our variables to it. 
+  pulseSensor.analogInput(PulseWire);   
+  pulseSensor.blinkOnPulse(LED13);       //auto-magically blink Arduino's LED with heartbeat.
+  pulseSensor.setThreshold(Threshold);   
+  pulseSensor.begin();
+  // Double-check the "pulseSensor" object was created and "began" seeing a signal. 
+}
+
+
+
+void loop() {
+  
+  if(pulseSensor.isInsideBeat()){
+    int myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
+    blePort.print(myBPM);
+    delay(5);
+    Serial.println(myBPM);                       // Print the value inside of myBPM. 
+  }
+  delay(20);                    // considered best practice in a simple sketch.
+
+}
+
+  
